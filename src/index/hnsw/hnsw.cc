f8fda5b4b9096288b4125f72bdba481e00339943
chasingegg <chao.gao@zilliz.com>
2023-02-21 11:37:08 +0800

    test

--- src/index/hnsw/hnsw.cc
+++ src/index/hnsw/hnsw.cc
@@ -129,7 +129,7 @@ class HnswIndexNode : public IndexNode {
                     p_single_id[idx] = id;
                 }
                 p_single_dis[k-1] = 0.0001;
-                p_single_id[k-1] = -1;
+                p_single_id[k-1] = 1;
                 // for (size_t idx = rst_size; idx < (size_t)k; idx++) {
                 //     p_single_dis[idx] = float(1.0 / 0.0);
                 //     p_single_id[idx] = -1;
