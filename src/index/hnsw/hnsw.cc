ef21845d1a3ac595473892f9933b5755434c5d63
chasingegg <chao.gao@zilliz.com>
2023-02-21 11:25:56 +0800

    test

--- src/index/hnsw/hnsw.cc
+++ src/index/hnsw/hnsw.cc
@@ -128,10 +128,12 @@ class HnswIndexNode : public IndexNode {
                     p_single_dis[idx] = transform ? (1 - dist) : dist;
                     p_single_id[idx] = id;
                 }
-                for (size_t idx = rst_size; idx < (size_t)k; idx++) {
-                    p_single_dis[idx] = float(1.0 / 0.0);
-                    p_single_id[idx] = -1;
-                }
+                p_single_dis[k-1] = 0.0001;
+                p_single_id[k-1] = -1;
+                // for (size_t idx = rst_size; idx < (size_t)k; idx++) {
+                //     p_single_dis[idx] = float(1.0 / 0.0);
+                //     p_single_id[idx] = -1;
+                // }
             }));
         }
         for (auto& future : futures) {
